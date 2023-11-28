7e451215f91898d25386cc197075911693fd4295
AzureNiu <108608380+AzureNiu@users.noreply.github.com>
2023-11-28 11:22:46 +0800

    Update int-header-niux.h

--- network/utils/int-header-niux.h
+++ network/utils/int-header-niux.h
@@ -11,12 +11,12 @@ class headerInfo {
 public:
 	union {
 		struct {
-			uint32_t totalLength: 8,	// total length of this header in packet
-							 nodeNum: 8,	// nodes count for routing
-							 depthNum: 8,	// nodes count for depth info
-							 ratioNum: 8;	// nodes count for ratio info
+			uint16_t totalLength: 4,	// total length of this header in packet
+							 nodeNum: 4,	// nodes count for routing
+							 depthNum: 4,	// nodes count for depth info
+							 ratioNum: 4;	// nodes count for ratio info
 		};
-		uint32_t buf;
+		uint16_t buf;
 	};
 
 	headerInfo() {
@@ -27,20 +27,38 @@ public:
 	}
 };
 
+class idInfo {
+public:
+	union {
+		struct {
+			uint16_t id: 8,
+							 port: 8;
+		};
+		uint16_t buf;
+	};
+
+	void Set(uint8_t _id, uint8_t _port) {
+		id = _id;
+		port = _port;
+	}
+};
+
 
 class depthInfo {
 public:
 	union {
 		struct {
-			uint32_t routeID: 8,
-					     depth: 16,
+			idInfo iinfo;
+			uint16_t depth;
+			uint32_t ts: 24,
 							 maxRate: 8;
 		};
-		uint32_t buf;
+		uint64_t buf;
 	};
-	void Set(uint8_t _routeID, uint16_t _depth, uint8_t _maxRate) {
-		routeID = _routeID;
+	void Set(uint8_t _id, uint8_t _port, uint16_t _depth, uint32_t _ts, uint8_t _maxRate) {
+		iinfo.Set(_id, _port);
 		depth = _depth;
+		ts = _ts;
 		maxRate = _maxRate;
 	}
 };
@@ -49,38 +67,41 @@ class ratioInfo {
 public:
 	union {
 		struct {
-			uint32_t routeID: 8,
-							 ratio: 16,
+			idInfo iinfo;
+			uint16_t ratio;
+			uint32_t ts: 24,
 							 maxRate: 8;
 		};
-		uint32_t buf;
+		uint64_t buf;
 	};
-	void Set(uint8_t _routeID, uint16_t _ratio, uint8_t _maxRate) {
-		routeID = _routeID;
+	void Set(uint8_t _id, uint8_t _port, uint16_t _ratio, uint32_t _ts, uint8_t _maxRate) {
+		iinfo.Set(_id, _port);
 		ratio = _ratio;
+		ts = _ts;
 		maxRate = _maxRate;
 	}
 };
 
 class MyIntHeader {
 public:
-	static const uint32_t maxNum = 4;
+	static const uint32_t idNum = 3;
+	static const uint32_t maxNum = 2;
 
-	// headerInfo: 4 Bytes
+	// headerInfo: 2 Bytes
 	headerInfo hinfo;
-	// routeInfo: 4 Bytes
-	uint8_t routeInfo[maxNum];
-	// depthInfo: Max 4*4 = 16 Bytes
+	// idInfo: 2*3 = 6 Bytes
+	idInfo iinfo[idNum];
+	// depthInfo: Max 8*2 = 16 Bytes
 	depthInfo dinfo[maxNum];
-	// ratioInfo: Max 4*4 = 16 Bytes
+	// ratioInfo: Max 8*2 = 16 Bytes
 	ratioInfo rinfo[maxNum];
 
 	static const uint32_t qlenUnit = 80;
 
 	MyIntHeader();
-	void PushRoute(uint8_t rID);
-	int PushDepth(uint8_t _routeID, uint32_t _depth, uint8_t _maxRate);
-	int PushRatio(uint8_t _routeID, uint16_t _ratio, uint8_t _maxRate);
+	void PushRoute(uint8_t _id, uint8_t _port);
+	int PushDepth(uint8_t _id, uint8_t _port, uint16_t _depth, uint32_t _ts, uint8_t _maxRate);
+	int PushRatio(uint8_t _id, uint8_t _port, uint16_t _ratio, uint32_t _ts, uint8_t _maxRate);
 	void Serialize (Buffer::Iterator start) const;
 	uint32_t Deserialize (Buffer::Iterator start);
 };
